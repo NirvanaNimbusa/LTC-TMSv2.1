69c12a38d223a0259d9a31d1a2d6084b5984483b
Nikolas Kevin <onymaruu@gmail.com>
2019-12-22 14:33:05 +0800

    Calendar update

--- CSC355-Browser-Team/Frontend/Schedulejs.js
+++ CSC355-Browser-Team/Frontend/Schedulejs.js
@@ -627,3 +627,51 @@ function openmenu(){
     document.getElementById("openmenu").style.opacity = ".6";
   }
 }
+
+// start of calender function 
+  
+
+document.addEventListener('DOMContentLoaded', function() {
+  var calendarEl = document.getElementById('calendar');
+
+  var calendar = new FullCalendar.Calendar(calendarEl, {
+
+    plugins: [ 'interaction', 'dayGrid', 'timeGrid', 'list', 'googleCalendar' ],
+
+    header: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+    },
+    navLinks: true, // can click day/week names to navigate views
+    editable: true,
+    eventLimit: true, // allow "more" link when too many events
+    businessHours: true,
+    displayEventTime: false, // don't show the time column in list view
+    
+    
+    // THIS KEY WON'T WORK WITH OTHER ACCOUNT!
+    // To make your own Google API key, follow the directions here:
+    // http://fullcalendar.io/docs/google_calendar/
+    googleCalendarApiKey: 'AIzaSyBMBwDqNEWKEKb2YIGEcE0h4AeJgEsVhkU',
+
+    events: 'mcultcver4@gmail.com',
+      
+    eventClick: function(arg) {
+      // opens events in a popup window
+      window.open(arg.event.url, 'google-calendar-event', 'width=700,height=600');
+
+      arg.jsEvent.preventDefault() // don't navigate in main tab
+    },
+    
+    loading: function(bool) {
+      document.getElementById('loading').style.display =
+        bool ? 'block' : 'none';
+    },
+
+  });
+  
+  calendar.render();
+});
+  
+// End of calender function
